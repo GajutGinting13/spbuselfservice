ef80f9f18494e704393bfb8972c184f156288da5
GajutGinting13 <GajutGinting13@users.noreply.github.com>
2024-07-21 12:20:59 +0700

    update

--- User/getdata.php
+++ User/getdata.php
@@ -2,3 +2,8 @@
 include '../database/koneksi.php';
 $sql = mysqli_query($koneksi, "SELECT * FROM proses limit 1");
 $data = mysqli_fetch_array($sql);
+if ($data) {
+    echo $data['nilai'];
+} else {
+    echo "0";
+}
