2c08d2876b908401bad8c310ba52aa34c69c68fa
GajutGinting13 <GajutGinting13@users.noreply.github.com>
2024-07-29 09:52:07 +0700

    updatr halaman admin

--- User/hapus.php
+++ User/hapus.php
@@ -1,3 +1,15 @@
 <?php
 include '../database/koneksi.php';
-mysqli_query($koneksi, "DELETE FROM proses");
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+
+$query = "DELETE FROM proses";
+if (mysqli_query($koneksi, $query)) {
+    echo "Data berhasil dihapus";
+} else {
+    echo "Error: " . $query . "<br>" . mysqli_error($koneksi);
+}
+
+mysqli_close($koneksi);
